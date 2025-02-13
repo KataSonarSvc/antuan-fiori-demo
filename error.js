3125fe95e7ddb8c3675a4165d85bd9eb94391513
KataSonarSvc <KatalonAdminGroup@abertis.com>
2025-02-13 09:40:07 +0100

    Update error.js

--- error.js
+++ error.js
@@ -1,1 +1,12 @@
-let images_folder= "images"
+var images_folder= "images"
+var pruebas = {
+    game_name: "Pruebas",
+    breadcrumb_name: "Pruebas",
+    breadcrumb_level1: breadcrumb_level1,
+    game_player_url: "player.html?SCREEN_FULLSCREEN_MODE=1",
+    description: "Carga básica del sistemas operativo MSX para pruebas.",
+    tricks: "Puedas cargar tus cassetes, disk o rom con los controles desde tu propio ordenador.",
+    game_mobile: "La misa funcionalidad desde el movil",
+    mobile_pad: "grey_pad.JPG",
+}
+
